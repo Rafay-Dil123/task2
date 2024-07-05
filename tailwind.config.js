7ae2e51166374b9e4bcf9741cba6263d97c7cc44
Rafay-Dil123 <dilrafay@gmail.com>
2024-07-05 15:08:29 +0500

    Made changes in navbar searchbar and footer to make it exactly like clone

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'roboto': ['Roboto', 'Roboto-fallback', 'Arial', 'sans-serif'],
+      },
+      fontSize: {
+        'inherit': 'inherit',
+      },
+      lineHeight: {
+        '1.5': '1.5',
+      },
+      fontWeight: {
+        'bold': '700',
+        'normal': '400',
+      },
+    },
   },
   plugins: [],
 }
